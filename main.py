65397c8fbf3bf766bf46d7ed29f29dba2e4aaeee
Himanshu <laddhahimanshu033@gmail.com>
2026-03-30 18:16:50 +0530

    Updated

---
+++ main.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from fastapi import FastAPI 
+from VDIIM import load_and_process_data 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI() 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+df, daily_df = load_and_process_data() 
+
+@app.get("/") 
+def home(): 
+    return {"message": "Nifty Volatility API is running"} 
+
+@app.get("/test") 
+def test(): 
+    return {"status": "working"} 
+
+@app.get("/volatility") 
+def get_volatility(limit: int = 100): 
+    data = df[["open", "high", "low", "close", "rolling_vol_15", "vol_spike", "range"]].reset_index().tail(limit)
+    return data.to_dict(orient="records")
+
+@app.get("/daily-summary")
+def daily_summary(limit: int = 20):
+    try:
+        data = daily_df.reset_index().tail(limit)
+        return data.to_dict(orient="records")
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.get("/high-vol-days")
+def high_vol_days(limit: int = 20):
+    data = daily_df[daily_df["regime"] == "High Vol"].reset_index().tail(limit)
+    return data.to_dict(orient = "records")
+
+@app.get("/intraday-by-date")
+def intraday_by_date(date: str):
+    try:
+        filtered = df[df.index.date == pd.to_datetime(date).date()]
+
+        data = filtered[["open", "high", "low", "close", "rolling_vol_15", "vol_spike", "range"]].reset_index()
+
+        return data.to_dict(orient = "records")
+    except Exception as e:
+        return {"error" : str(e)}
